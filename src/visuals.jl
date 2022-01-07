48fdfc82bdb853e906ed21105bdf716d87eb9dd2
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2022-01-07 14:37:30 +0000

    Improves plotting method
    Previous approach was taking ~15 secs to plot large areas. New approach takes less than 0.1 secs.

--- src/visuals.jl
+++ src/visuals.jl
@@ -1,20 +1,23 @@
-rectangle(x, y, w, h) = Shape(x .+ [0, w, w, 0], y .+ [0, 0, h, h])
-
 function plot_area(area)
-    xu, yu = size(area)
+    area_w, area_h = size(area)
+    plot_w, plot_h = 600, 600
+    marker_w = 0.5 * plot_w / area_w
 
     p = plot(
-        aspect_ratio = :equal, size = (400, 400),
-        xlims = (0.5, yu + 0.5), ylims = (0.5, xu + 0.5),
-        xtick = 0:5:yu, ytick = 0:5:xu, tickfontsize = 4,
+        aspect_ratio = :equal, size = (plot_w, plot_h),
+        xlims = (0.5, area_h + 0.5), ylims = (0.5, area_w + 0.5),
+        xtick = 0:5:area_h, ytick = 0:5:area_w, tickfontsize = 4,
         legend = nothing, yflip = true, palette = :Pastel1,
     )
 
-    for ci in CartesianIndices(area)
-        (x, y), w, h = ci.I, 1, 1
-        r = rectangle(y - 0.5, x - 0.5, h, w)
-        plot!(p, r, color = Int(area[ci]), line = :white, linewidth = 0.5)
-    end
+    points = mapreduce(ci -> [ci.I...], hcat, CartesianIndices(area))
+    colors = map(ci -> Int(area[ci]), CartesianIndices(area)) |> vec
+
+    scatter!(
+        p, points[2, :], points[1, :], color = colors,
+        markershape = :rect, markerstrokecolor = :white,
+        markersize = marker_w, markerstrokewidth = 0.5,
+    )
 
     p
 end
