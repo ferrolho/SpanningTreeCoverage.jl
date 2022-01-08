0ab17e227657afbb37d10eb9d80e658e06e872d7
Henrique Ferrolho <henrique.ferrolho@gmail.com>
2022-01-08 00:08:55 +0000

    Adds convenience function `read_solve_plot`

--- src/SpanningTreeCoverage.jl
+++ src/SpanningTreeCoverage.jl
@@ -9,4 +9,19 @@ include("io.jl")
 include("planning.jl")
 include("visuals.jl")
 
+"""
+Read an _area_ from `filename`, solve the
+single-robot Coverage Path Planning (CPP), and
+then plot the area and the optimal coverage path.
+"""
+function read_solve_plot(filename::AbstractString)
+    area = read_area(filename)
+    path = solve(area)
+    new_area = subdivide_area(area)
+    p = plot_area(new_area)
+    plot_path!(p, path)
+end
+
+export read_solve_plot
+
 end # module
